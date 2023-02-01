af488c5eba506fc57f684bba5c32403431ce3906
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-02-01 12:52:43 +0100

    js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import star from '../src/imgs/star.png';
+import React, { useState } from 'react';
 
 
 
 
       function Submit (){
   return(
-    <button id='submit'>submit</button>
+    <button id='submit' onClick={submitAnwser}>submit</button>
   )
 }
 
 function Numbers(){
   return(
     <div className='numbers'>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
+      <div className='choice1' onClick={anwser1}>1</div>
+      <div className='choice2' onClick={anwser2}>2</div>
+      <div className='choice3' onClick={anwser3}>3</div>
+      <div className='choice4' onClick={anwser4}>4</div>
+      <div className='choice5' onClick={anwser5}>5</div>
     </div>
   )
 }
@@ -58,4 +59,29 @@ function App() {
 
 export default App;
 
-const submit = document.getElementById('submit');
+//variables////////////////
+const submit = document.querySelector('#submit');
+const num1 = document.querySelector('.choice1');
+const num2 = document.querySelector('.choice2');
+const num3 = document.querySelector('.choice3');
+const num4 = document.querySelector('.choice4');
+const num5 = document.querySelector('.choice5');
+//event listening
+function anwser1(){
+ 
+}
+function anwser2(){
+  
+}
+function anwser3(){
+  
+}
+function anwser4(){
+  
+}
+function anwser5(){
+  
+}
+function submitAnwser(){
+
+}
